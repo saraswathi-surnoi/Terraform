09c670ae6b0580796cb80c2eaa58fb415f6bab8a
saraswathi-surnoi <saraswathinandula@surnoi.com>
2024-08-29 15:43:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,61 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = var.aws_region
+  region = "ap-south-1"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "172.16.0.0/16"
-  instance_tenancy = "default"
+resource "aws_instance" "webserver" {
+  count                  = length(var.availability_zones)
+  ami                    = "vars.ami"
+  instance_type          = "vars.instance_type
+  key_name               = "vars.key_name
+  vpc_security_group_ids = ["sg-02d1a43a4d4424949"]
+  availability_zone      = var.availability_zones[count.index]
+
   tags = {
-    Name = "main"
-  }
-}
-
-#Create security group with firewall rules
-resource "aws_security_group" "jenkins-sg-2022" {
-  name        = var.security_group
-  description = "security group for Ec2 instance"
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- # outbound from jenkis server
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags= {
-    Name = var.security_group
-  }
-}
-
-resource "aws_instance" "myFirstInstance" {
-  ami           = var.ami_id
-  key_name = var.key_name
-  instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
-  tags= {
-    Name = var.tag_name
+    Name = var.server_names[count.index]
   }
 }
 
 # Create Elastic IP address
-resource "aws_eip" "myFirstInstance" {
+resource "aws_eip" "webserver" {
   vpc      = true
-  instance = aws_instance.myFirstInstance.id
+  instance = aws_instance.webserver.id
 tags= {
     Name = "my_elastic_ip"
   }
