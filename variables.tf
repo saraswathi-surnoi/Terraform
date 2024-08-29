9e7443ac5f671745cc61a96e27cc670db70ee2be
saraswathi-surnoi <saraswathinandula@surnoi.com>
2024-08-29 16:50:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,7 +20,7 @@ variable "security_group" {
 
 variable "tag_name" { 
     description = "Tag Name of for Ec2 instance" 
-    default     = "my-jenkins" 
+    default     = "myFirstInstance" 
 } 
 variable "ami_id" { 
     description = "AMI for linux Ec2 instance" 
