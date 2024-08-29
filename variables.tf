eed2c3346b6b67ba3917705bd71dcebd8a243494
saraswathi-surnoi <saraswathinandula@surnoi.com>
2024-08-29 15:44:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,51 +1,20 @@
-variable "aws_region" {
-       description = "The AWS region to create things in." 
-       default     = "us-east-1" 
+variable "availability_zones" {
+  type    = list(string)
+  default = ["ap-south-1a", "ap-south-1b"]
 }
-
-variable "key_name" { 
-    description = " SSH keys to connect to ec2 instance" 
-    default     =  "mySep22Key" 
+variable "server_names" {
+  type    = list(string)
+  default = ["webserver-1", "webserver-2"]
 }
-
-variable "instance_type" { 
-    description = "instance type for ec2" 
-    default     =  "t2.micro" 
+variable "ami" {
+  default = "ami-025fe52e1f2dc5044"
 }
-
-variable "security_group" { 
-    description = "Name of security group" 
-    default     = "jenkins-sgroup-dec-2021" 
+variable "instance_type" {
+  default = "t2.micro"
 }
-
-variable "tag_name" { 
-    description = "Tag Name of for Ec2 instance" 
-    default     = "my-ec2-instance" 
-} 
-variable "ami_id" { 
-    description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0ee23bfc74a881de5" 
+variable "key_name" {
+  default = "demo-pp"
 }
-variable "versioning" {
-    type        = bool
-    description = "(Optional) A state of versioning."
-    default     = true
-}
-variable "acl" {
-    type        = string
-    description = " Defaults to private "
-    default     = "private"
-}
-variable "bucket_prefix" {
-    type        = string
-    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
-    default     = "my-s3bucket-"
-}
-variable "tags" {
-    type        = map
-    description = "(Optional) A mapping of tags to assign to the bucket."
-    default     = {
-        environment = "DEV"
-        terraform   = "true"
-    }
+variable "ami" {
+  default = "ami-025fe52e1f2dc5044"
 }
