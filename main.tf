31f5d789a6e706c0567bc5a95bb7ae6380890389
saraswathi-surnoi <saraswathinandula@surnoi.com>
2024-08-30 10:50:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,3 +52,11 @@ tags= {
     Name = "my_elastic_ip"
   }
 }
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Replace with your AMI ID
+  instance_type = var.instance_types[var.application_type]
+
+  tags = {
+    Name = var.application_type
+  }
+}
