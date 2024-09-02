85825d61b4f4d6cd48ed0f44554984d1c6f307b2
saraswathi-surnoi <saraswathinandula@surnoi.com>
2024-09-02 19:27:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ tags= {
   }
 }
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Replace with your AMI ID
+  ami           = "ami-02b49a24cfb95941c"  # Replace with your AMI ID
   instance_type = lookup(var.instance_types, var.application_type")
 
   tags = {
